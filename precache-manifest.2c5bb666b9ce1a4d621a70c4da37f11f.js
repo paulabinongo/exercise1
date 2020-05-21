e6bc12ef04700418f7ac9d52c5994da541130420
Paula Marie B. Binongo <paula.binongo@98labs.com>
2020-05-21 11:47:59 +0800

    Updates

---
+++ precache-manifest.2c5bb666b9ce1a4d621a70c4da37f11f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d2c7c5f9a26956cb814e03b20c9b55",
+    "url": "/exercise1/index.html"
+  },
+  {
+    "revision": "c860982ef05967ba74d7",
+    "url": "/exercise1/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "188242848d59316efbf7",
+    "url": "/exercise1/static/js/2.4ad92371.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/exercise1/static/js/2.4ad92371.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c860982ef05967ba74d7",
+    "url": "/exercise1/static/js/main.9fa3b600.chunk.js"
+  },
+  {
+    "revision": "302e7fa444b1bb5a08cc",
+    "url": "/exercise1/static/js/runtime-main.7809a373.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/exercise1/static/media/logo.5d5d9eef.svg"
+  }
+]);
